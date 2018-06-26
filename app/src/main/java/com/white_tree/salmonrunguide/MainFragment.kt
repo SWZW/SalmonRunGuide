9d9c50be244c5845e5ad57d6fb7fae48e8df496b
swzwshiroki <swzwshiroki@gmail.com>
2018-06-26 23:32:10 +0900

    [add]OnCreateメソッドの追加
    MainFragmentのOverrideメソッドOnCreateを追加しました。

--- app/src/main/java/com/white_tree/salmonrunguide/MainFragment.kt
+++ app/src/main/java/com/white_tree/salmonrunguide/MainFragment.kt
@@ -1,6 +1,10 @@
 package com.white_tree.salmonrunguide
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
 
 class MainFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
