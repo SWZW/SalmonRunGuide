a49182522cb2fecce4e5b011458619c62d947c26
swzwshiroki <swzwshiroki@gmail.com>
2018-06-26 23:32:56 +0900

    [add]OnCreateViewメソッドの追加
    MainFragmentのOverrideメソッドOnCreateViewを追加しました。

--- app/src/main/java/com/white_tree/salmonrunguide/MainFragment.kt
+++ app/src/main/java/com/white_tree/salmonrunguide/MainFragment.kt
@@ -2,9 +2,16 @@ package com.white_tree.salmonrunguide
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class MainFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
